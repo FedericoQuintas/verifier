48ee0e4434355d354f1e855fb1dc638400610aaf
fede-at-616462008038 <cPw/21Olg2jWc8Pa4pSGalpyoiVYcBamfLYmAGb9FGo=>
2023-06-25 17:42:21 +0200

    Create table automatically

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS order_events (
 id bigint NOT NULL,
-side VARCHAR(5),
-quantity int,
-price DECIMAL(10,2),
-event_type VARCHAR(30),
-event_time timestamp
+side VARCHAR(5) NOT NULL,
+quantity int NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+event_type VARCHAR(30) NOT NULL,
+event_time timestamp NOT NULL
 );
 
 CREATE INDEX index_id ON order_events (id);
